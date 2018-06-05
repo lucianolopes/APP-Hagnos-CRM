c3192a6b869e2f8cfb675b1521c7e73b9aafd9d8
lucianolopes <lvieira.lopes@hotmail.com>
2018-06-05 14:17:16 -0300

    Add files via upload

--- www/js/kitchen-sink.js
+++ www/js/kitchen-sink.js
@@ -735,24 +735,24 @@ myApp.onPageInit('menu2', function (page) {
         $$(this).find('.swiper-container')[0].swiper.update();
     });
     
-    if (usuarioHagnos.hagnosUsuarioTipo == 2){
+    if (tipousuario == 2){
         $$(".esconde-rep").hide();
     }
-    if (usuarioHagnos.hagnosUsuarioTipo == 3){
+    if (tipousuario == 3){
         $$(".esconde-cliente").hide();   
     }
 
     var rp = "";
-    if (usuarioHagnos.hagnosUsuarioTipo == 1 || usuarioHagnos.hagnosUsuarioTipo == 2){
+    if (tipousuario == 1 || tipousuario == 2){
         
 
-        if (usuarioHagnos.hagnosUsuarioTipo == 1){
+        if (tipousuario == 1){
         $$(".esconde-admin").hide(); 
         }
 
-        if (usuarioHagnos.hagnosUsuarioTipo == 2){
+        if (tipousuario == 2){
         $$(".esconde-rep").hide(); 
-        var rp = usuarioHagnos.hagnosUsuarioIdRep;
+        var rp = rep;
         }
         
 
